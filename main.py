bf57422eb35b0649cb96493f0fee20b8a5a92137
Lucas Rosa <lucasrsilva1604@gmail.com>
2021-03-17 18:59:33 -0300

    added testTrainer function to main

--- main.py
+++ main.py
@@ -13,13 +13,15 @@ def testReader():
         print(si.getGold())
         sleep(1)
 
-
-if __name__ == '__main__':
+def testTrainer():
     c = Controller(DDragon())
     print(c.pool)
     print(c.odds)
-    shop = c.getShop(1)
+    shop = c.refreshShop([None, None, None, None, None], 1)
     for i in range(9):
         print(shop)
         shop = c.refreshShop(shop, i + 1)
+
+if __name__ == '__main__':
+    testTrainer()
     # testReader()
