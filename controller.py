665e341e523977826744af5927309e964588a098
Lucas Rosa <lucasrsilva1604@gmail.com>
2021-05-26 22:36:44 -0300

    adding new set data

--- controller.py
+++ controller.py
@@ -1,14 +1,14 @@
 import random
 from database import DDragon
 
-class Controller:
 
+class Controller:
     # Format:
     # pool = {
     #     cost: [{
     #         championInfo,
-    #         ammount,
-    #         maxAmmount
+    #         amount,
+    #         maxAmount
     #     }]
     # }
     pool = {
@@ -27,50 +27,58 @@ class Controller:
     # }
     odds = {}
 
-    def changePoolAmmount(self, shop, func):
+    def changePoolAmount(self, shop, func):
         for cost, championsByCost in self.pool.items():
             for i in range(len(championsByCost)):
-                self.pool[cost][i]["ammount"] = func(championsByCost[i]["ammount"], shop.count(championsByCost[i]["championInfo"]["name"]))
+                self.pool[cost][i]["amount"] = func(championsByCost[i]["amount"],
+                                                    shop.count(championsByCost[i]["championInfo"]["name"]))
 
     def generateShop(self, level):
         oddsByCost = self.odds[level]
         choices = []
         for cost in range(1, 6):
-            odd = oddsByCost[cost]/100
+            odd = oddsByCost[cost] / 100
             for champion in self.pool[cost]:
-                choices += [champion["championInfo"]["name"]]*int(champion["ammount"]*odd)
+                choices += [champion["championInfo"]["name"]] * int(champion["amount"] * odd)
         shop = []
         for i in range(5):
             if len(choices) > 0:
                 shop.append(random.choices(choices)[0])
             else:
                 shop.append(None)
-        self.changePoolAmmount(shop, lambda a, b: a-b)
+        self.changePoolAmount(shop, lambda a, b: a - b)
         return shop
 
     def refreshShop(self, oldShop, level):
-        self.changePoolAmmount(oldShop, lambda a, b: a+b)
+        self.changePoolAmount(oldShop, lambda a, b: a + b)
         return self.generateShop(level)
 
-    def __init__(self):
+    def __init__(self, load=False):
+
         database = DDragon()
+
+        if load:
+            database.request()
+            database.load()
 
         poolSize = {}
         for key, value in database.pool.items():
             poolSize[int(key)] = value
 
         for champion in database.champions:
-            if len(champion["traits"]) > 0:
+            if champion["cost"] > 5:
+                continue
+            if len(champion["traits"]) > 0 and champion["cost"] <= 5:
                 self.pool[champion["cost"]].append({
                     "championInfo": champion,
-                    "ammount": poolSize[champion["cost"]],
-                    "maxAmmount": poolSize[champion["cost"]],
+                    "amount": poolSize[champion["cost"]],
+                    "maxAmount": poolSize[champion["cost"]],
                 })
             else:
                 self.pool[champion["cost"]].append({
                     "championInfo": champion,
-                    "ammount": 0,
-                    "maxAmmount": 0,
+                    "amount": 0,
+                    "maxAmount": 0,
                 })
 
         for level, oddsByCost in database.odds.items():
